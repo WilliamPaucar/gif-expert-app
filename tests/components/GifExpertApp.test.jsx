e86a0dce5eaaf491730b4a0514e164572959b150
William Alejandro C.m <crazzy.wili1990@hotmail.com>
2025-02-18 21:15:40 -0500

    Probando el componente principal GifExpertApp

--- tests/components/GifExpertApp.test.jsx
+++ tests/components/GifExpertApp.test.jsx
@@ -1,10 +1,17 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import {  GifExpertApp} from "../../src/GifExpertApp";
 describe('Probando el componente principal <GifExpertApp/>', () => { 
     
-    test('debe returnar la estructura principal', () => {
-        render(<GifExpertApp/>)
-        screen.debug();
-     })
+    test('debe cambiar el valor del input al teclear', () => { 
+
+        render(<GifExpertApp />);
+        // extraemos el valor del input 
+        const input = screen.getByRole('textbox');
+        // disparamos el evento onChange y enviamos el vamor de One punch al input
+        fireEvent.input(input,{target: {value:'One Punch'}});
+        // verificamos si el input a cambiado al valor enviado One Punch
+        expect(input.value).toBe('One Punch');
+        
+    })
  })
 
